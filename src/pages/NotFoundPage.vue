770eba422cd984bd4e9d9dd0f15b1ccd9b6cff38
fadhilzamanNow <fadhilisfadhillah@gmail.com>
2025-10-02 00:48:54 +0700

    feat : add not found page

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { BugOutlined } from "@ant-design/icons-vue";
+import { Button } from "ant-design-vue";
+</script>
+
+<template>
+  <div class="h-screen w-full flex flex-col items-center justify-center gap-4">
+    <BugOutlined class="text-7xl" />
+    <div class="text-center flex flex-col items-center justify-center gap-2">
+      <h1 class="text-4xl font-bold">404</h1>
+      <p class="text-xl">Halaman yang kamu cari di FindSoed gak ada.... :(</p>
+      <RouterLink to="/home">
+        <Button type="primary">Kembali</Button>
+      </RouterLink>
+    </div>
+  </div>
+</template>
--- src/router.ts
+++ src/router.ts
@@ -7,62 +7,69 @@ import {
 import { defineAsyncComponent } from "vue";
 import Loading from "./components/Loading/Loading.vue";
 
-
 const routes = [
   {
     path: "/",
     name: "landing",
     component: defineAsyncComponent({
-      loader : () => import("./pages/LandingPage.vue"),
-      loadingComponent : Loading
+      loader: () => import("./pages/LandingPage.vue"),
+      loadingComponent: Loading,
     }),
   },
   {
     path: "/login",
     name: "login",
     component: defineAsyncComponent({
-      loader : () => import("./pages/LoginPage.vue"),
-      loadingComponent : Loading
-    })
+      loader: () => import("./pages/LoginPage.vue"),
+      loadingComponent: Loading,
+    }),
   },
   {
     path: "/register",
     name: "about",
     component: defineAsyncComponent({
-      loader : () => import("./pages/RegisterPage.vue"),
-      loadingComponent : Loading
+      loader: () => import("./pages/RegisterPage.vue"),
+      loadingComponent: Loading,
     }),
   },
   {
     path: "/home",
     name: "list",
     component: defineAsyncComponent({
-      loader : () => import("./pages/ListPage.vue"),
-      loadingComponent : Loading
+      loader: () => import("./pages/ListPage.vue"),
+      loadingComponent: Loading,
     }),
   },
   {
     path: "/add",
     name: "add",
     component: defineAsyncComponent({
-      loader : () => import("./pages/AddPage.vue"),
-      loadingComponent : Loading
+      loader: () => import("./pages/AddPage.vue"),
+      loadingComponent: Loading,
     }),
   },
   {
     path: "/setting",
     name: "setting",
     component: defineAsyncComponent({
-      loader : () => import("./pages/SettingsPage.vue"),
-      loadingComponent : Loading
+      loader: () => import("./pages/SettingsPage.vue"),
+      loadingComponent: Loading,
     }),
   },
   {
     path: "/detail/:id",
     name: "detail",
     component: defineAsyncComponent({
-      loader : () => import("./pages/DetailItemPage.vue"),
-      loadingComponent : Loading
+      loader: () => import("./pages/DetailItemPage.vue"),
+      loadingComponent: Loading,
+    }),
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: "notfound",
+    component: defineAsyncComponent({
+      loader: () => import("./pages/NotFoundPage.vue"),
+      loadingComponent: Loading,
     }),
   },
 ];
