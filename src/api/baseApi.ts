08766e786cf76aedeb3b38864835455a3f3af6e1
fadhilzamanNow <fadhilisfadhillah@gmail.com>
2025-10-01 19:36:13 +0700

    fix : fix url

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -12,7 +12,7 @@ export type CustomSuccessResponse = {
 };
 
 const basePath = axios.create({
-  baseURL: "http://ec2-54-255-245-74.ap-southeast-1.compute.amazonaws.com:3500",
+  baseURL: "https://find-soed-rework-express.vercel.app",
   timeout: 10000,
 });
 
